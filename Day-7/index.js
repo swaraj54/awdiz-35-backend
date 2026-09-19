90ce346aeb17d0b3aed254c2ad60b94f2f2e24d1
Swaraj <jadhavswaraj1920@gmail.com>
2026-09-19 15:06:27 +0530

    updates for day 9.

--- Day-7/index.js
+++ Day-7/index.js
@@ -1,10 +1,29 @@
 import express from "express";
 import mainRouter from "./routes/mainRouter.js";
 import { errorHandler } from "./middlewares/errorMiddlewares.js";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import dns from "node:dns";
+
+dns.setServers([
+  "8.8.8.8",
+  "1.1.1.1"
+]);
 
 const app = express();
 
 app.use(express.json());
+dotenv.config();
+// console.log(process.env.MONGODBURL,"process.env.MONGODBURL")
+
+mongoose
+  .connect(process.env.MONGODBURL)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.get("/", (req, res) => {
   res.send("Welcome to the API");
